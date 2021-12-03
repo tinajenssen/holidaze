6c6f9943d6b53b891ce8b9f59f487e1535e6b4d9
tinajenssen <63742433+tinajenssen@users.noreply.github.com>
2021-12-03 15:50:57 +0100

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6f3a62b0120b46c8e175218f434ecfd7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '89a9875e-4887-4d22-9bb6-5a52ad30cfc4'
+};
